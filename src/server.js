62162b3ee7e637fce188b147351034bf19e09614
Redegit <dima070603@gmail.com>
2024-08-30 00:07:20 +0300

    change group & add classes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const path = require('path');
-const axios = require('axios');
-const cors = require('cors');
+const express = require("express");
+const path = require("path");
+const axios = require("axios");
+const cors = require("cors");
+const https = require("https");
 
 const app = express();
 const port = 3001;
@@ -11,32 +12,45 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Подгрузка статических файлов из папки build
-app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(express.static(path.join(__dirname, "..", "build")));
 
 // Обработка запроса к корневому URL
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
 // Разрешаем запросы для dev-сервера
 const corsOptions = {
-  origin: '*',
+  origin: "*",
 };
 
 // Middleware для обработки CORS
 app.use(cors(corsOptions));
 
+const groupAliases = {
+  "ПИ21-5": 137271,
+  "ПИ21-2": 110809,
+};
+
 // Обработка запроса для проксирования
-app.get('/api/ruz', async (req, res) => {
+app.get("/api/ruz", async (req, res) => {
   try {
-    const targetUrl = "https://ruz.fa.ru/api/schedule/group/110809/";
-    const startDate = req.query.start || '';
-    const endDate = req.query.finish || '';
-    const lng = req.query.lng || '1';
+    const targetUrl =
+      "https://ruz.fa.ru/api/schedule/group/" + groupAliases["ПИ21-5"];
+    const startDate = req.query.start || "";
+    const endDate = req.query.finish || "";
+    const lng = req.query.lng || "1";
 
     const fullUrl = `${targetUrl}?start=${startDate}&finish=${endDate}&lng=${lng}`;
-    const response = await axios.get(fullUrl);
+    const response = await axios
+      .get(fullUrl, {
+        rejectUnauthorized: false,
+        httpsAgent: new https.Agent({
+          rejectUnauthorized: false,
+        }),
+      })
     const data = { data: response.data };
+
     res.json(data);
   } catch (error) {
     res.status(500).json({ error: error.message });
